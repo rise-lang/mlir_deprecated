7dd955f17e0d04049e3857dbb9baa122773e134a
Martin Lücke <martin.luecke@ed.ac.uk>
2019-09-23 15:48:45 +0100

    TypeStorages now go into TypeDetail.h

--- include/mlir/Dialect/Lift/Types.h
+++ include/mlir/Dialect/Lift/Types.h
@@ -14,8 +14,6 @@
 /////////////////////// Custom Types for the Lift Dialect ///////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-using llvm::ArrayRef;
-
 namespace lift {
 
 namespace detail {
@@ -25,9 +23,56 @@ struct LiftArrayTypeStorage;
 /// LLVM-style RTTI: one entry per subclass to allow dyn_cast/isa.
 enum LiftTypeKind {
     // The enum starts at the range reserved for this dialect.
-    LIFT_TYPE = mlir::Type::FIRST_LIFT_TYPE,
+            LIFT_KIND = mlir::Type::FIRST_LIFT_TYPE,
+    LIFT_NAT,
+    LIFT_DATA,
     LIFT_ARRAY,
 };
+
+class Kind : public mlir::Type::TypeBase<Kind, mlir::Type> {
+public:
+    /// Inherit some necessary constructors from 'TypeBase'.
+    using Base::Base;
+
+    /// This static method is used to support type inquiry through isa, cast,
+    /// and dyn_cast.
+    static bool kindof(unsigned kind) { return kind == LiftTypeKind::LIFT_KIND; }
+
+    /// This method is used to get an instance of the 'SimpleType'. Given that
+    /// this is a parameterless type, it just needs to take the context for
+    /// uniquing purposes.
+    static Kind get(mlir::MLIRContext *context) {
+        // Call into a helper 'get' method in 'TypeBase' to get a uniqued instance
+        // of this type.
+        return Base::get(context, LiftTypeKind::LIFT_KIND);
+    }
+};
+
+class Nat : public mlir::Type::TypeBase<Nat, Kind> {
+public:
+    /// Inherit some necessary constructors from 'TypeBase'.
+    using Base::Base;
+
+    /// This static method is used to support type inquiry through isa, cast,
+    /// and dyn_cast.
+    static bool kindof(unsigned kind) { return kind == LiftTypeKind::LIFT_NAT; }
+
+    /// This method is used to get an instance of the 'SimpleType'. Given that
+    /// this is a parameterless type, it just needs to take the context for
+    /// uniquing purposes.
+    static Kind get(mlir::MLIRContext *context) {
+        // Call into a helper 'get' method in 'TypeBase' to get a uniqued instance
+        // of this type.
+        return Base::get(context, LiftTypeKind::LIFT_NAT);
+    }
+};
+
+
+
+
+
+
+
 
 /// Type for Lift arrays.
 /// In MLIR Types are reference to immutable and uniqued objects owned by the
@@ -38,6 +83,7 @@ class LiftArrayType : public mlir::Type::TypeBase<LiftArrayType, mlir::Type,
         detail::LiftArrayTypeStorage> {
 
 public:
+    /// Inherit some necessary constructors from 'TypeBase'.
     using Base::Base;
 
     /// Returns the dimensions for this array, or and empty range for a generic
--- lib/Dialect/Lift/IR/Types.cpp
+++ lib/Dialect/Lift/IR/Types.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "mlir/Dialect/Lift/Types.h"
+#include "mlir/Dialect/Lift/TypeDetail.h"
 #include "mlir/Dialect/Lift/Dialect.h"
 
 #include "mlir/IR/Builders.h"
@@ -21,50 +22,10 @@ using llvm::StringRef;
 using llvm::Twine;
 
 namespace lift {
-namespace detail {
 
-/// This class holds the implementation of the LiftArrayType.
-/// It is intended to be uniqued based on its content and owned by the context.
-struct LiftArrayTypeStorage : public mlir::TypeStorage {
-    /// This defines how we unique this type in the context: our key contains
-    /// only the shape, a more complex type would have multiple entries in the
-    /// tuple here.
-    /// The element of the tuples usually matches 1-1 the arguments from the
-    /// public `get()` method arguments from the facade.
-    using KeyTy = std::tuple<ArrayRef<int64_t>>;
-
-    static unsigned hashKey(const KeyTy &key) {
-        return llvm::hash_combine(std::get<0>(key));
-    }
-
-    /// When the key hash hits an existing type, we compare the shape themselves
-    /// to confirm we have the right type.
-    bool operator==(const KeyTy &key) const { return key == KeyTy(getShape()); }
-
-    /// This is a factory method to create our type storage. It is only
-    /// invoked after looking up the type in the context using the key and not
-    /// finding it.
-    static LiftArrayTypeStorage *construct(mlir::TypeStorageAllocator &allocator,
-                                           const KeyTy &key) {
-        // Copy the shape array into the bumpptr allocator owned by the context.
-        ArrayRef<int64_t> shape = allocator.copyInto(std::get<0>(key));
-
-        // Allocate the instance for the LiftArrayTypeStorage itself
-        auto *storage = allocator.allocate<LiftArrayTypeStorage>();
-        // Initialize the instance using placement new.
-        return new(storage) LiftArrayTypeStorage(shape);
-    }
-
-    ArrayRef<int64_t> getShape() const { return shape; }
-
-private:
-    ArrayRef<int64_t> shape;
-
-    /// Constructor is only invoked from the `construct()` method above.
-    LiftArrayTypeStorage(ArrayRef<int64_t> shape) : shape(shape) {}
-};
-
-} // namespace detail
+//===----------------------------------------------------------------------===//
+// LiftArrayType
+//===----------------------------------------------------------------------===//
 
 mlir::Type LiftArrayType::getElementType() {
     return mlir::FloatType::getF64(getContext());
